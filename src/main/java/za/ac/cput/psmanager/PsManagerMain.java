0eaf39e671b1596c75de741e2c857cfbe282019f
Franciel Sebastião <219466912@mycput.ac.za>
2021-06-07 15:10:07 +0200

    my first commit

---
+++ src/main/java/za/ac/cput/psmanager/PsManagerMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.psmanager;
+
+
+/**
+ *
+ * @author jodykearns
+ * @author FrancielSebastiao
+ * @author JadeKennemeyer
+ */
+public class PsManagerMain {
+    public static void main(String[] args) {
+        
+        new InventoryManager().runInventoryManager();
+        
+    }
+}
